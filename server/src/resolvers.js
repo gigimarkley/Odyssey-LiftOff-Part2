9c766fd23d3d2bb7cd2efecc7d5a27b216f569d1
Gigi Markley <gigimarkley@Admins-MacBook-Pro.local>
2021-10-29 21:29:14 -0700

    Implementing query resolvers

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,7 +3,14 @@ const resolvers = {
     //returns array of tracks that will be used to populate
     //the homepage grid of web client
     //This below is a resolver function
-    tracksForHome: (parent, args, context, info) => {},
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
   },
 };
 module.exports = resolvers;
